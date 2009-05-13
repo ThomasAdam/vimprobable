b06e65b56faaf5ad8bcd8924895b79228f6e8eb3
Leon Winter <lwi@ring0.de>
2009-05-13 17:59:35 +0200

    initial config.h

---
+++ config.h
@@ -0,0 +1,14 @@
+/*
+    (c) 2009 by Leon Winter, see LICENSE file
+*/
+
+/* general settings */
+static const char startpage[]           = "https://projects.ring0.de/webkitbrowser/";
+static const char userstylesheet[]      = NULL;
+
+/* appearance */
+static const char sslbgcolor            = "#b0ff00";    /* background color for SSL */
+
+/* scrolling */
+static unsigned int scrollstep          = 40;   /* cursor difference in pixel */
+static unsigned int pagingkeep          = 40;   /* pixels kept when paging */
