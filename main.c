22f54f0569e5eebf228198a2e8f0f4c81efc6caa
Leon Winter <lwi@ring0.de>
2009-05-13 21:44:22 +0200

    add further callback stubs and fix compiler warnings

--- main.c
+++ main.c
@@ -16,7 +16,8 @@ enum { TargetCurrent, TargetNew };                                      /* targe
 
 /* callbacks here */
 static void window_destroyed_cb(GtkWidget* window, gpointer func_data);
-static void webview_title_changed(WebKitWebView* webview, WebKitWebFrame* frame, char* title, gpointer user_data);
+static void webview_title_changed_cb(WebKitWebView* webview, WebKitWebFrame* frame, char* title, gpointer user_data);
+static void webview_load_committed_cb(WebKitWebView* webview, WebKitWebFrame* frame, gpointer user_data);
 
 /* functions */
 static void setup_gui();
@@ -38,7 +39,17 @@ window_destroyed_cb(GtkWidget* window, gpointer func_data) {
 }
 
 void
-webview_title_changed(WebKitWebView* webview, WebKitWebFrame* frame, char* title, gpointer user_data) {
+webview_title_changed_cb(WebKitWebView* webview, WebKitWebFrame* frame, char* title, gpointer user_data) {
+
+}
+
+void
+webview_progress_changed_cb(WebKitWebView* webview, int progress, gpointer user_data) {
+
+}
+
+void
+webview_load_committed_cb(WebKitWebView* webview, WebKitWebFrame* frame, gpointer user_data) {
 
 }
 
@@ -51,22 +62,22 @@ setup_gui() {
     adjust_h = gtk_range_get_adjustment((GtkRange*)scroll_h);
     adjust_v = gtk_range_get_adjustment((GtkRange*)scroll_v);
     GtkWidget* window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    GtkBox* box = gtk_vbox_new(FALSE, 0);
+    GtkBox* box = (GtkBox*)gtk_vbox_new(FALSE, 0);
     input = gtk_entry_new();
     GtkWidget* viewport = gtk_scrolled_window_new(adjust_h, adjust_v);
-    webview = webkit_web_view_new();
-    WebKitWebSettings* settings = webkit_web_settings_new();
+    webview = (WebKitWebView*)webkit_web_view_new();
+    WebKitWebSettings* settings = (WebKitWebSettings*)webkit_web_settings_new();
 
     // let config.h set up WebKitWebSettings
     gtk_widget_set_name(window, "WebKitBrowser");
 #ifdef DISABLE_SCROLLBAR
     gtk_scrolled_window_set_policy((GtkScrolledWindow*)viewport, GTK_POLICY_NEVER, GTK_POLICY_NEVER);
 #endif
-    setup_signals(window, webview);
+    setup_signals((GObject*)window, (GObject*)webview);
     gtk_container_add((GtkContainer*)viewport, (GtkWidget*)webview);
     gtk_box_pack_start(box, input, FALSE, FALSE, 0);
     gtk_box_pack_start(box, viewport, TRUE, TRUE, 0);
-    gtk_container_add((GtkContainer*)window, box);
+    gtk_container_add((GtkContainer*)window, (GtkWidget*)box);
     gtk_widget_grab_focus((GtkWidget*)webview);
     gtk_widget_show_all(window);
     webkit_web_view_load_uri(webview, startpage);
@@ -75,10 +86,12 @@ setup_gui() {
 
 void
 setup_signals(GObject* window, GObject* webview) {
+    /* window */
     g_signal_connect((GObject*)window,  "destroy", (GCallback)window_destroyed_cb, NULL);
-
-    g_signal_connect((GObject*)webview, "title-changed", (GCallback)webview_title_changed, NULL);
-    //g_signal_connect((GObject*)webview, "", (GCallback)webview_title_changed, NULL);
+    /* webview */
+    g_signal_connect((GObject*)webview, "title-changed", (GCallback)webview_title_changed_cb, NULL);
+    g_signal_connect((GObject*)webview, "load-progress-changed", (GCallback)webview_progress_changed_cb, NULL);
+    g_signal_connect((GObject*)webview, "load-committed", (GCallback)webview_load_committed_cb, NULL);
 }
 
 int
