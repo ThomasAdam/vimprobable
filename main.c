d18cc40dd8e3fba3cc78d782c3bed0f38c5a0997
Leon Winter <lwi@ring0.de>
2009-05-13 17:40:33 +0200

    example structure

---
+++ main.c
@@ -0,0 +1,26 @@
+/*
+    (c) 2009 by Leon Winter, see LICENSE file
+*/
+
+#include <stdlib.h>
+#include <string.h>
+#include <gtk/gtk.h>
+#include <gdk/gdkkeysyms.h>
+#include <webkit/webkit.h>
+#include <libsoup/soup.h>
+
+enum { ModeNormal, ModeInsert, ModeSearch, ModeWebsearch, ModeHints };  /* modes */
+enum { TargetCurrent, TargetNew };                                      /* target */
+
+/* structs here */
+
+/* func defines here */
+
+#include "config.h"
+
+/* funcs here */
+
+int
+main(int argc, char* argv[]) {
+    return EXIT_SUCCESS;
+}
