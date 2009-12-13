f6036e06ed9e189bdc6a078c0daf49ab0ea3f576
Hannes Schueller <hannes@yllr.net>
2009-12-13 11:56:18 +0100

    fixing downloads not initiated by right-click

--- config.h
+++ config.h
@@ -64,7 +64,7 @@ static const char progressborderright   = ']';
 #define             DEFAULT_FONT_SIZE           12
 
 /* user agent */
-#define             USER_AGENT                  "Vimprobable 0.9.7.1"
+#define             USER_AGENT                  "Vimprobable 0.9.7.2"
 
 /* scrolling */
 static unsigned int scrollstep          = 40;   /* cursor difference in pixel */
