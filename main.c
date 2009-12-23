9797131de22b07dea07f16e7642e1283074228d2
Hannes Schueller <hannes@yllr.net>
2009-12-23 12:21:06 +0100

    minor code cleanup (by Matto)

--- main.c
+++ main.c
@@ -456,17 +456,21 @@ gboolean
 inputbox_keypress_cb(GtkEntry *entry, GdkEventKey *event) {
     Arg a;
 
-    if(event->keyval == GDK_Escape) {
-        a.i = HideCompletion;
-        complete(&a);
-        a.i = ModeNormal;
-        return set(&a);
-    } else if(event->keyval == GDK_Tab) {
-        a.i = DirectionNext;
-        return complete(&a);
-    } else if(event->keyval == GDK_ISO_Left_Tab) {
-        a.i = DirectionPrev;
-        return complete(&a);
+     switch (event->keyval) {
+         case GDK_Escape:
+             a.i = HideCompletion;
+             complete(&a);
+             a.i = ModeNormal;
+             return set(&a);
+         break;
+         case GDK_Tab:
+             a.i = DirectionNext;
+             return complete(&a);
+         break;
+         case GDK_ISO_Left_Tab:
+             a.i = DirectionPrev;
+             return complete(&a);
+         break;
     }
     return FALSE;
 }
