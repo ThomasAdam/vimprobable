89103f563b790f6c2d636e9e74f1fbf68b1aa187
Hannes Schueller <hannes@yllr.net>
2010-04-07 23:42:08 +0200

    increasing version number

--- config.h
+++ config.h
@@ -7,7 +7,7 @@
 
 /* general settings */
 char startpage[241]                     = "http://www.yllr.net/vimprobable/";
-char useragent[120]	                    = "Vimprobable2 0.8.1.2";
+char useragent[120]	                    = "Vimprobable2 0.8.1.3";
 static const gboolean enablePlugins     = TRUE; /* TRUE keeps plugins enabled */
 
 /* appearance */
