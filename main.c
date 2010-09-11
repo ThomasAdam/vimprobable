497a9066c3d8051c02ee804894157a6fcd488c44
Thomas Adam <thomas@xteddy.org>
2010-09-11 20:22:19 +0100

    Fix segfault with ":bma" command.
    When the tagging option was introduced, this broke the :bma command by
    always assuming there would be a tag option after the name --- but
    previously no option after this command meant the current URL.
    
    This patch restores that behaviour.

--- main.c
+++ main.c
@@ -1505,7 +1505,7 @@ bookmark(const Arg *arg) {
             fprintf(f, "%s", " ");
             fprintf(f, "%s", title);
         }
-        if (strlen(arg->s)) { 
+        if (arg->s && strlen(arg->s)) {
             build_taglist(arg, f);
         }
         fprintf(f, "%s", "\n");
