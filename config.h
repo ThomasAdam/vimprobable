65ed3dac76c7db2a3d1c3e4b05496322e4a9eba0
Hannes Schueller <hannes@yllr.net>
2010-09-14 14:05:29 +0200

    version number

--- config.h
+++ config.h
@@ -7,7 +7,7 @@
 
 /* general settings */
 char startpage[241]                     = "http://www.vimprobable.org/";
-char useragent[120]	                    = "Vimprobable2/0.9.3.1";
+char useragent[120]	                    = "Vimprobable2/0.9.3.2";
 static const gboolean enablePlugins     = TRUE; /* TRUE keeps plugins enabled */
 
 /* appearance */
