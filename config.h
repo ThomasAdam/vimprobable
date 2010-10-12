f9e19256f3588136b03f196decfffd18808c39b1
Hannes Schueller <hannes@yllr.net>
2010-10-12 11:13:24 +0200

    version 0.9.5.0

--- config.h
+++ config.h
@@ -7,7 +7,7 @@
 
 /* general settings */
 char startpage[241]                     = "http://www.vimprobable.org/";
-char useragent[120]	                    = "Vimprobable2/0.9.4.0";
+char useragent[120]	                    = "Vimprobable2/0.9.5.0";
 static const gboolean enablePlugins     = TRUE; /* TRUE keeps plugins enabled */
 static const gboolean enableJava        = TRUE; /* FALSE disables Java applets */
 
