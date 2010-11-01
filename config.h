30a72c9484c5fa15006591eb6b98cccf5e666f72
Hannes Schueller <hannes@yllr.net>
2010-11-01 21:13:55 +0100

    Replacement for last broken commit message:
    Porting Thomas Adams' cookie handling revamp from version 2.
    His commit message:
    
    Currently, Vimprobable implements cookies by only referencing the cookie-jar
    when that specific instance of Vimprobable loads -- if there's more than one
    Vimprobable instance which then sets cookies, there's no way of other
    instances to "share" cookies.  This is annoying on sites like gmail, where
    despite having a gmail window open, clicking a link to go to the calendar,
    will force the user to reauthenticate.
    
    So instead, parse the headers of each request (via libsoup) to conditionally
    set the cookie, by always looking in our cookie-jar first to see if it's
    available, and if so, use it.

--- config.h
+++ config.h
@@ -1,7 +1,9 @@
 /*
     (c) 2009 by Leon Winter
-    (c) 2009 by Hannes Schueller
-    (c) 2009 by Matto Fransen
+    (c) 2009-2010 by Hannes Schueller
+    (c) 2009-2010 by Matto Fransen
+    (c) 2010 by Hans-Peter Deifel
+    (c) 2010 by Thomas Adams
     see LICENSE file
 */
 
