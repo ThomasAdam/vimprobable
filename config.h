38fac669b47bac63a9d979d2803ff3f4d2fbedd4
Hannes Schueller <hannes@yllr.net>
2010-11-28 16:02:51 +0100

    version 0.9.20.4

--- config.h
+++ config.h
@@ -70,7 +70,7 @@ static const char progressborderright   = ']';
 #define             DEFAULT_FONT_SIZE           12
 
 /* user agent */
-#define             USER_AGENT                  "Vimprobable/0.9.20.3"
+#define             USER_AGENT                  "Vimprobable/0.9.20.4"
 
 /* scrolling */
 static unsigned int scrollstep          = 40;   /* cursor difference in pixel */
