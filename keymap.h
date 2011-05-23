8eeb42f37ee3ad4d17df7b2ecf469785de7089bd
Albert Kim <alkim1234@gmail.com>
2011-05-22 20:47:04 -0700

    Changed hint mode start key from ` to period.
    Also changed key to follow in new window from ~ to comma.

--- keymap.h
+++ keymap.h
@@ -101,15 +101,15 @@ Key keys[] = {
     { 0,                    0,              GDK_KP_Divide,  input,      {.s = "/"} },
     { GDK_SHIFT_MASK,       0,              GDK_question,   input,      {.s = "?"} },
 
-    { 0,                    0,              GDK_quoteleft,  input,      {.s = "`"} },
-    { GDK_SHIFT_MASK,       0,              GDK_asciitilde, input,      {.s = "~"} },
+    { 0,                    0,              GDK_period,     input,      {.s = "."} },
+    { 0,                    0,              GDK_comma,      input,      {.s = ","} },
 
     { 0,                    GDK_VoidSymbol, GDK_Escape,     set,        {ModeNormal} },
     { GDK_CONTROL_MASK,     GDK_VoidSymbol, GDK_bracketleft,set,        {ModeNormal} },
     { GDK_CONTROL_MASK,     0,              GDK_z,          set,        {ModePassThrough} },
     { GDK_CONTROL_MASK,     0,              GDK_v,          set,        {ModeSendKey} },
-    { 0,                    0,              GDK_f,          input,      {.s = "`"} },
-    { GDK_SHIFT_MASK,       0,              GDK_F,          input,      {.s = "~"} },
+    { 0,                    0,              GDK_f,          input,      {.s = "."} },
+    { GDK_SHIFT_MASK,       0,              GDK_F,          input,      {.s = ","} },
 
     { 0,                    GDK_g,          GDK_i,          focus_input,{} },
     { 0,                    0,              GDK_u,          revive,     {} },
