e3096e16d0dd71e83c5e722819c5a81eab9abb7d
Daniel Carl <danielcarl@gmx.de>
2011-08-11 23:49:28 +0200

    Enabled hinting for framepages too.
    This might be useful for later hintingactions, like yanking link urls,
    download target. The opening of links within frame doesn't work like
    expectet, because the url will be opened by the c layer that doen't know
    anything about the target of the hint.

--- hinting.js
+++ hinting.js
@@ -6,86 +6,116 @@
 */
 function Hints() {
     var hintContainer;
-    var hintCount;
     var currentFocusNum = 1;
 
     /* hints[] = [elem, number, text, span, backgroundColor, color] */
-    var hints = [];
+    var hints;
 
     this.createHints = function(inputText)
     {
-        if (document.getElementsByTagName("body")[0] === null || typeof(document.getElementsByTagName("body")[0]) != "object")
-            return;
+        var topwin = window;
+        var top_height = topwin.innerHeight;
+        var top_width = topwin.innerWidth;
+        var xpath_expr;
 
-        var height = window.innerHeight;
-        var width = window.innerWidth;
-        var scrollX = document.defaultView.scrollX;
-        var scrollY = document.defaultView.scrollY;
-        _generateHintContainer();
-
-        /* prefixing html: will result in namespace error */
-        var hinttags;
-        if (typeof(inputText) == "undefined" || inputText == "") {
-            hinttags = "//*[@onclick or @onmouseover or @onmousedown or @onmouseup or @oncommand or @class='lk' or @role='link' or @href] | //input[not(@type='hidden')] | //a | //area | //iframe | //textarea | //button | //select";
-        } else {
-            /* only elements which match the text entered so far */
-            hinttags = "//*[(@onclick or @onmouseover or @onmousedown or @onmouseup or @oncommand or @class='lk' or @role='link' or @href) and contains(., '" + inputText + "')] | //input[not(@type='hidden') and contains(., '" + inputText + "')] | //a[contains(., '" + inputText + "')] | //area[contains(., '" + inputText + "')] | //iframe[contains(@name, '" + inputText + "')] | //textarea[contains(., '" + inputText + "')] | //button[contains(@value, '" + inputText + "')] | //select[contains(., '" + inputText + "')]";
-        }
-
-        /*
-        iterator type isn't suitable here, because: "DOMException NVALID_STATE_ERR:
-        The document has been mutated since the result was returned."
-        */
-        var r = document.evaluate(hinttags, document,
-            function(p) {
-                return "http://www.w3.org/1999/xhtml";
-            }, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-
-        /* generate basic hint element which will be cloned and updated later */
-        var hintSpan = document.createElement("span");
-        hintSpan.setAttribute("class", "hinting_mode_hint");
-        hintSpan.style.position = "absolute";
-        hintSpan.style.background = "red";
-        hintSpan.style.color = "#fff";
-        hintSpan.style.font = "bold 10px monospace";
-        hintSpan.style.zIndex = "10000000";
-
-        /* due to the different XPath result type, we will need two counter variables */
-        this.hintCount = 0;
-        var i;
+        var hintCount = 0;
         hints = [];
-        for (i = 0; i < r.snapshotLength; i++)
-        {
-            var elem = r.snapshotItem(i);
-            var rect = elem.getBoundingClientRect();
-            if (!rect || rect.top > height || rect.bottom < 0 || rect.left > width || rect.right < 0 || !(elem.getClientRects()[0]))
-                continue;
-            var computedStyle = document.defaultView.getComputedStyle(elem, null);
-            if (computedStyle.getPropertyValue("visibility") != "visible" || computedStyle.getPropertyValue("display") == "none")
-                continue;
-            var leftpos = Math.max((rect.left + scrollX), scrollX);
-            var toppos = Math.max((rect.top + scrollY), scrollY);
 
-            /* process elements text */
-            var text = _getTextFromElement(elem);
+        function helper (win, offsetX, offsetY) {
+            var doc = win.document;
 
-            /* making this block DOM compliant */
-            var hint = hintSpan.cloneNode(false);
-            hint.setAttribute("id", "vimprobablehint" + this.hintCount);
-            hint.style.left = leftpos + "px";
-            hint.style.top =  toppos + "px";
-            var text = document.createTextNode(this.hintCount + 1);
-            hint.appendChild(text);
-            hintContainer.appendChild(hint);
-            this.hintCount++;
-            hints.push([elem, this.hintCount, text, hint, elem.style.background, elem.style.color]);
-            /* make the link black to ensure it's readable */
-            elem.style.color = "#000";
-            elem.style.background = "#ff0";
+            var win_height = win.height;
+            var win_width = win.width;
+
+            /* Bounds */
+            var minX = offsetX < 0 ? -offsetX : 0;
+            var minY = offsetY < 0 ? -offsetY : 0;
+            var maxX = offsetX + win_width > top_width ? top_width - offsetX : top_width;
+            var maxY = offsetY + win_height > top_height ? top_height - offsetY : top_height;
+
+            var scrollX = win.scrollX;
+            var scrollY = win.scrollY;
+
+            hintContainer = doc.createElement("div");
+            hintContainer.id = "hint_container";
+
+            if (typeof(inputText) == "undefined" || inputText == "") {
+                xpath_expr = "//*[@onclick or @onmouseover or @onmousedown or @onmouseup or @oncommand or @class='lk' or @role='link' or @href] | //input[not(@type='hidden')] | //a | //area | //textarea | //button | //select";
+            } else {
+                xpath_expr = "//*[(@onclick or @onmouseover or @onmousedown or @onmouseup or @oncommand or @class='lk' or @role='link' or @href) and contains(., '" + inputText + "')] | //input[not(@type='hidden') and contains(., '" + inputText + "')] | //a[contains(., '" + inputText + "')] | //area[contains(., '" + inputText + "')] | //iframe[contains(@name, '" + inputText + "')] | //textarea[contains(., '" + inputText + "')] | //button[contains(@value, '" + inputText + "')] | //select[contains(., '" + inputText + "')]";
+            }
+
+            var res = doc.evaluate(xpath_expr, doc,
+                function (p) {
+                    return "http://www.w3.org/1999/xhtml";
+                }, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+
+            /* generate basic hint element which will be cloned and updated later */
+            var hintSpan = doc.createElement("span");
+            hintSpan.setAttribute("class", "hinting_mode_hint");
+            hintSpan.style.position = "absolute";
+            hintSpan.style.background = "red";
+            hintSpan.style.color = "#fff";
+            hintSpan.style.font = "bold 10px monospace";
+            hintSpan.style.zIndex = "10000000";
+
+            /* due to the different XPath result type, we will need two counter variables */
+            var rect, elem, text, node, show_text;
+            for (var i = 0; i < res.snapshotLength; i++)
+            {
+                elem = res.snapshotItem(i);
+                rect = elem.getBoundingClientRect();
+                if (!rect || rect.left > maxX || rect.right < minX || rect.top > maxY || rect.bottom < minY)
+                    continue;
+
+                var style = topwin.getComputedStyle(elem, "");
+                if (style.display == "none" || style.visibility != "visible")
+                    continue;
+
+                var leftpos = Math.max((rect.left + scrollX), scrollX);
+                var toppos = Math.max((rect.top + scrollY), scrollY);
+
+                /* process elements text */
+                text = _getTextFromElement(elem);
+
+                /* making this block DOM compliant */
+                var hint = hintSpan.cloneNode(false);
+                hint.setAttribute("id", "vimprobablehint" + hintCount);
+                hint.style.left = leftpos + "px";
+                hint.style.top =  toppos + "px";
+                text = doc.createTextNode(hintCount + 1);
+                hint.appendChild(text);
+
+                hintContainer.appendChild(hint);
+                hintCount++;
+                hints.push([elem, hintCount, text, hint, elem.style.background, elem.style.color]);
+
+                /* make the link black to ensure it's readable */
+                elem.style.color = "#000";
+                elem.style.background = "#ff0";
+            }
+
+            doc.documentElement.appendChild(hintContainer);
+
+            /* recurse into any iframe or frame element */
+            var frameTags = ["frame","iframe"];
+            for (var i in frameTags) {
+                var frames = doc.getElementsByTagName(frameTags[i]);
+                for (var i = 0, nframes = frames.length; i < nframes; ++i) {
+                    elem = frames[i];
+                    rect = elem.getBoundingClientRect();
+                    if (!elem.contentWindow || !rect || rect.left > maxX || rect.right < minX || rect.top > maxY || rect.bottom < minY)
+                        continue;
+                    helper(elem.contentWindow, offsetX + rect.left, offsetY + rect.top);
+                }
+            }
         }
+
+        helper(topwin, 0, 0);
+
         this.clearFocus();
         this.focusHint(1);
-        if (this.hintCount == 1) {
+        if (hintCount == 1) {
             /* just one hinted element - might as well follow it */
             return this.fire(1);
         }
@@ -169,6 +199,7 @@ function Hints() {
             if (typeof(hint[0]) != "undefined") {
                 hint[0].style.background = hint[4];
                 hint[0].style.color = hint[5];
+                hint[3].parentNode.removeChild(hint[3]);
             }
         }
         hintContainer.parentNode.removeChild(hintContainer);
@@ -177,6 +208,7 @@ function Hints() {
 
     this.fire = function(n)
     {
+        var doc;
         if (!n) {
             var n = currentFocusNum;
         }
@@ -187,6 +219,7 @@ function Hints() {
         var el = hint[0];
         var tag = el.nodeName.toLowerCase();
         this.clearHints();
+
         if (tag == "iframe" || tag == "frame" || tag == "textarea" || tag == "input" && (el.type == "text" || el.type == "password" || el.type == "checkbox" || el.type == "radio") || tag == "select") {
             el.focus();
             return;
@@ -195,8 +228,9 @@ function Hints() {
             /* send signal to open link */
             return "open;" + el.href;
         }
-        var evObj = document.createEvent("MouseEvents");
-        evObj.initMouseEvent("click", true, true, window, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, null);
+        doc = el.ownerDocument;
+        var evObj = doc.createEvent("MouseEvents");
+        evObj.initMouseEvent("click", true, true, el.contentWindow, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, null);
         el.dispatchEvent(evObj);
     };
 
@@ -238,26 +272,11 @@ function Hints() {
             first.focus();
     };
 
-    function _generateHintContainer()
-    {
-        var body = document.getElementsByTagName("body")[0];
-        if (document.getElementById("hint_container")) {
-            return;
-        }
-
-        hintContainer = document.createElement("div");
-        hintContainer.id = "hint_container";
-
-        if (body)
-            body.appendChild(hintContainer);
-    }
-
     function _getTextFromElement(el)
     {
-        var tagname = el.tagName.toLowerCase();
-        if (tagname == "input" || tagname == "textarea") {
+        if (el instanceof HTMLInputElement || el instanceof HTMLTextAreaElement) {
             text = el.value;
-        } else if (tagname == "select") {
+        } else if (el instanceof HTMLSelectElement) {
             if (el.selectedIndex >= 0) {
                 text = el.item(el.selectedIndex).text;
             } else{
@@ -299,7 +318,7 @@ function Hints() {
         if (hint[1] == n) {
             hint[0].style.background = hint[4];
             hint[0].style.color = hint[5];
-            hintContainer.removeChild(hint[3]);
+            hint[3].parentNode.removeChild(hint[3]);
 
             /* remove hints from all hints */
             hints.splice(index, 1);
