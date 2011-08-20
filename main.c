0c25239fd197818a8b8191e81c2496e2721fe91a
Daniel Carl <danielcarl@gmx.de>
2011-08-20 15:41:41 +0200

    Added return value insert; to notify c code about insert mode.

--- main.c
+++ main.c
@@ -624,14 +624,13 @@ static gboolean inputbox_changed_cb(GtkEditable *entry, gpointer user_data) {
         switch (text[0]) {
             case '.':
                 a.s = g_strconcat("hints.createHints('", text + 1, "', 'f');", NULL);
-                count = 0;
                 break;
 
             case ',':
                 a.s = g_strconcat("hints.createHints('", text + 1, "', 'F');", NULL);
-                count = 0;
                 break;
         }
+        count = 0;
         script(&a);
 
         return TRUE;
@@ -960,14 +959,13 @@ input(const Arg *arg) {
         switch (arg->s[0]) {
             case '.':
                 a.s = g_strdup("hints.createHints('', 'f');");
-                count = 0;
                 break;
 
             case ',':
                 a.s = g_strdup("hints.createHints('', 'F');");
-                count = 0;
                 break;
         }
+        count = 0;
         script(&a);
     }
 
@@ -1352,6 +1350,16 @@ script(const Arg *arg) {
         a.s = g_strdup(value);
         echo(&a);
         g_free(a.s);
+    }
+    /* switch mode according to scripts return value */
+    if (value) {
+        if (strncmp(value, "done;", 5) == 0) {
+            a.i = ModeNormal;
+            set(&a);
+        } else if (strncmp(value, "insert;", 7) == 0) {
+            a.i = ModeInsert;
+            set(&a);
+        }
     }
     if (arg->s)
         g_free(arg->s);
